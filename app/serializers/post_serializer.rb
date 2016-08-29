4320195cbe4aa81ed5c7695dca6f628aaa2b93e9
Takahiro Ikeda <takahiro.ikeda@livesense.co.jp>
2016-08-29 15:55:34 +0900

    wip

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,7 +2,7 @@ class PostSerializer < ApplicationSerializer
   attributes :id, :title, :body
 
   belongs_to :user do
-    Comment.new(name: "hoge")
+    Comment.new(name: 'hoge')
   end
 
   def body
